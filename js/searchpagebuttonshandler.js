1157257a0572c2d6d7884a3a671dd22d4f967f72
sanketcouragedigital <sanketdhotre@couragedigital.com>
2015-08-26 11:16:30 +0530

    location, config and uber image add

--- js/searchpagebuttonshandler.js
+++ js/searchpagebuttonshandler.js
@@ -1,21 +1,21 @@
 $(function() {
-	$( "#calculateOptions" ).click(function(){
-			
-		var source = $("#source").val();
-		if(source === "Current Location"){
-			sessionStorage.setItem("origin", window.origin);
-		}else{			
-			sessionStorage.setItem("origin",source);
-		}
-		var destination=$("#dest").val();
-		var timeOfPickup=$("input[name*=radio-choice]:checked").val();
-		
-		sessionStorage.setItem("dest",destination);
-		sessionStorage.setItem("timeOfPickup",timeOfPickup);
-		window.location="cheapestridesearchresults.html";
-	});
+    $( "#calculateOptions" ).click(function(){
+            
+        var source = $("#source").val();
+        if(source === "Current Location"){
+            sessionStorage.setItem("origin", window.origin);
+        }else{          
+            sessionStorage.setItem("origin",source);
+        }
+        var destination=$("#dest").val();
+        var timeOfPickup=$("input[name*=radio-choice]:checked").val();
+        
+        sessionStorage.setItem("dest",destination);
+        sessionStorage.setItem("timeOfPickup",timeOfPickup);
+        window.location="cheapestridesearchresults.html";
+    });
 
-	//When user hits the "user current location option"
+    //When user hits the "user current location option"
         $("#useCurrentLocation").bind("click", function(event, ui){
 
             $.mobile.loading( "show", {
@@ -25,43 +25,55 @@ $(function() {
                   html: ""
             }); 
             //Get the current latitude and longitude.
-                if(navigator.geolocation) {
-                    $("#calculateOptions").button("disable");
-						navigator.geolocation.getCurrentPosition(function(position) {
+            //getCurrentLocation();
+            checkGPSStateAndGetCurrentLocation();
+            
+        });
+        
+        function checkGPSStateAndGetCurrentLocation(){
+            cordova.plugins.diagnostic.isLocationEnabled(function(enabled){
+                if(enabled) {
+                    calcuateCurrentLocationCoordinates();
+                }
+                else{
+                    $.mobile.loading( "hide" );
+                    //to turn on your Gps
+                    $("#dlg-location-error").popup("open");
+                }
+            }, onError);
+        }
+        function onError(error){
+            alert("An error occurred: "+error);
+        }
+
+
+        function calcuateCurrentLocationCoordinates() {
+             if(navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                        
                         var latitude = parseFloat(position.coords.latitude).toFixed(4);
                         var longitude = parseFloat(position.coords.longitude).toFixed(4);
 
                         window.origin = new google.maps.LatLng(latitude,longitude);
                         $("#source").val("Current Location");
-                        $("#calculateOptions").button("enable");
-                        $.mobile.loading( "hide" ); 
+                        $.mobile.loading( "hide" );
                     },
-					function (error) {
-						switch(error.code) {
-							case error.PERMISSION_DENIED:
-								alert("Please turn on your location.");
-								
-							break;
-							case error.POSITION_UNAVAILABLE:
-								alert("Location information is unavailable.");
-								
-								break;
-							casealert( "The request to get user location timed out.");
-							
-								break;
-							case error.UNKNOWN_ERROR:
-								alert("An unknown error occurred.");
-								
-								break;
-						}$.mobile.loading( "hide" );
-				});
+                    function (error) {
+                        $.mobile.loading( "hide" );
+                        //to set high accuracy (location setting)
+                        $("#dlg-location-highAccuracy-error").popup("open");
+                    },
                     
-                } else {
-                    //console.log("Current location Not Found !");
-                    $("#source").val("latitude and longitude not found");
-                    $.mobile.loading( "hide" ); 
+                    {
+                        timeout: 10000, enableHighAccuracy: true
+                    });
+                    
+                    //setTimeout(checkState, 500);
                 }
-        }); 
-		
-		
+        }
+
+        $( "#locationError, #locationHighAccuracyError" ).click(function(){
+            cordova.plugins.diagnostic.switchToLocationSettings();
+        });
+
 });
