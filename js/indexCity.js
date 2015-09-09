de0e3ce97929024545d1e64d6205e41c848d6a36
sanketcouragedigital <sanketdhotre@couragedigital.com>
2015-09-09 12:50:11 +0530

    version 1 final code

--- js/indexCity.js
+++ js/indexCity.js
@@ -1,8 +1,20 @@
 $(function() {
 	$("#selectCity").on("change",function(){
 		var selectedCity=$("#selectCity").val();
+		sessionStorage.setItem("city",selectedCity);
 		if(selectedCity=="Mumbai"){
 			window.location="mainindex.html";
 		}
 	});
+	
+	$("#wifienable").click(function() {
+		cordova.plugins.diagnostic.switchToWifiSettings();
+	});
+	$("#dataenable").click(function() {
+		cordova.plugins.diagnostic.switchToMobileDataSettings();
+	}); 
+	$("#cancel").click(function() {
+		navigator.app.exitApp();
+	}); 
+
 });
